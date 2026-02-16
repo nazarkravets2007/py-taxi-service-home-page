836b7c8ed0e17fa55b02535e32b92c682469cff4
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-02-16 13:19:06 +0200

    Update views.py

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Driver, Car, Manufacturer
+from taxi.models import Driver, Car, Manufacturer
 
 def index(request):
     num_drivers = Driver.objects.count()
