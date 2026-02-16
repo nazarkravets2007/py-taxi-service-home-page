33990da837f04aeccd0f572f60453a409e439d35
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-02-16 13:08:13 +0200

    Update views.py

--- taxi/views.py
+++ taxi/views.py
@@ -3,12 +3,13 @@ from .models import Driver, Car, Manufacturer
 
 def index(request):
     num_drivers = Driver.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
 
     context = {
-        "num_drivers": num_drivers,
-        "num_manufacturers": num_manufacturers,
-        "num_cars": num_cars,
+        'num_drivers': num_drivers,
+        'num_cars': num_cars,
+        'num_manufacturers': num_manufacturers,
     }
-    return render(request, "taxi/index.html", context)
+
+    return render(request, 'taxi/index.html', context)
